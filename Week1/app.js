947f624f97360f053882a399aac05cea10ccd638
Ali Ahdab <aliahdab@gmail.com>
2019-11-26 21:45:03 +0100

    hello

--- Week1/app.js
+++ Week1/app.js
@@ -12,7 +12,7 @@ const connection = mysql.createConnection({
 app.get('/q1', (req, res) => {
   console.log('Question Number 1')
   console.log('What are the names of countries with population greater than 8 million')
-  connection.query('SELECT name,population FROM country where population>8000000', (err, rows, fields) => {
+  connection.query('SELECT name as CountryName,population as Population FROM country where population>8000000', (err, rows, fields) => {
     res.json(rows)
   })
 })
@@ -20,10 +20,35 @@ app.get('/q1', (req, res) => {
 app.get('/q2', (req, res) => {
   console.log('Question Number 2')
   console.log('What are the names of countries that have “land” in their names ?')
-  connection.query('SELECT name FROM country WHERE name LIKE "%land%"', (err, rows, fields) => {
+  connection.query('SELECT name as CountryName FROM country WHERE name LIKE "%land%"', (err, rows, fields) => {
     res.json(rows)
   })
 })
+
+app.get('/q3', (req, res) => {
+  console.log('Question Number 3')
+  console.log('What are the names of the cities with population in between 500,000 and 1 million ?')
+  connection.query('SELECT name as CountryName,population FROM city WHERE population>500000 and population<1000000', (err, rows, fields) => {
+    res.json(rows)
+  })
+})
+
+app.get('/q4', (req, res) => {
+  console.log('Question Number 4')
+  console.log("What's the name of all the countries on the continent ‘Europe’ ?")
+  connection.query("SELECT name FROM country WHERE continent='Europe'", (err, rows, fields) => {
+    res.json(rows)
+  })
+})
+
+app.get('/q5', (req, res) => {
+  console.log('Question Number 5')
+  console.log("List all the countries in the descending order of their surface areas.")
+  connection.query("SELECT name as CountryName ,SurfaceArea FROM country order by SurfaceArea desc ", (err, rows, fields) => {
+    res.json(rows)
+  })
+})
+
 app.get("/", (req, res) => {
   console.log("responding to root route")
   res.send("Hello Alooosh >>>> here you can find the answer for week1 database questions")
@@ -31,5 +56,5 @@ app.get("/", (req, res) => {
 
 // localhost:888
 app.listen(888, () => {
-  console.log("Server is up and listening om 888")
+  console.log("Server is up and listening on 888")
 })
