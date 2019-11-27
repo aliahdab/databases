0b3f3d11cdbe08f200707fd203b89355b8a4df3f
Ali Ahdab <aliahdab@gmail.com>
2019-11-27 22:10:09 +0100

    add some code

--- Week1/app.js
+++ Week1/app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const app = express();
 const mysql = require('mysql');
+
 const connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
@@ -10,53 +11,76 @@ const connection = mysql.createConnection({
 })
 
 app.get('/q1', (req, res) => {
-  console.log('Question Number 1')
-  console.log('What are the names of countries with population greater than 8 million')
   connection.query('SELECT name as CountryName,population as Population FROM country where population>8000000', (err, rows, fields) => {
+    if (err) { throw err; }
     res.json(rows)
   })
 })
 
 app.get('/q2', (req, res) => {
-  console.log('Question Number 2')
-  console.log('What are the names of countries that have “land” in their names ?')
+
   connection.query('SELECT name as CountryName FROM country WHERE name LIKE "%land%"', (err, rows, fields) => {
+    if (err) { throw err; }
     res.json(rows)
   })
 })
 
 app.get('/q3', (req, res) => {
-  console.log('Question Number 3')
-  console.log('What are the names of the cities with population in between 500,000 and 1 million ?')
   connection.query('SELECT name as CountryName,population FROM city WHERE population>500000 and population<1000000', (err, rows, fields) => {
+    if (err) { throw err; }
     res.json(rows)
   })
 })
 
 app.get('/q4', (req, res) => {
-  console.log('Question Number 4')
-  console.log("What's the name of all the countries on the continent ‘Europe’ ?")
   connection.query("SELECT name FROM country WHERE continent='Europe'", (err, rows, fields) => {
+    if (err) { throw err; }
     res.json(rows)
   })
 })
 
 app.get('/q5', (req, res) => {
-  console.log('Question Number 5')
-  console.log("List all the countries in the descending order of their surface areas.")
   connection.query("SELECT name as CountryName ,SurfaceArea FROM country order by SurfaceArea desc ", (err, rows, fields) => {
+    if (err) { throw err; }
     res.json(rows)
   })
 })
 
 app.get('/q6', (req, res) => {
-  console.log('Question Number 6')
-  console.log("What are the names of all the cities in the Netherlands?")
   connection.query("SELECT name as CountryName ,SurfaceArea FROM country order by SurfaceArea desc ", (err, rows, fields) => {
+    if (err) { throw err; }
     res.json(rows)
   })
 })
 
+app.get('/q7', (req, res) => {
+  connection.query("SELECT name,population FROM city where name='Rotterdam' ", (err, rows, fields) => {
+    if (err) { throw err; }
+    res.json(rows)
+  })
+})
+
+app.get('/q8', (req, res) => {
+  connection.query("SELECT name,SurfaceArea FROM country order by SurfaceArea desc LIMIT 10", (err, rows, fields) => {
+    if (err) { throw err; }
+    res.json(rows)
+  })
+})
+
+app.get('/q9', (req, res) => {
+  connection.query("SELECT name,population FROM city order by population desc LIMIT 10", (err, rows, fields) => {
+    if (err) { throw err; }
+    res.json(rows)
+  })
+
+})
+
+app.get('/q10', (req, res) => {
+  connection.query("select sum(population) as Population_OF_The_World_is fromf country", (err, rows, fields) => {
+    if (err) { throw err; }
+    res.json(rows)
+  })
+})
 
 
 app.get("/", (req, res) => {
