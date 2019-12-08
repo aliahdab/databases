910f1f55bbc7287d56e0b059997be42f31c133ee
Ali Ahdab <aliahdab@gmail.com>
2019-12-08 09:37:15 +0100

    hello

--- Week2/app.js
+++ Week2/app.js
@@ -1,5 +1,21 @@
 // get the client
 const mysql = require('mysql2');
+const prompts = require('prompts');
+
+
+
+(async () => {
+  const response = await prompts({
+    type: 'number',
+    name: 'value',
+    message: 'How old are you?',
+    validate: value => value < 18 ? `Nightclub is 18+ only` : true
+  });
+
+  console.log(response); // => { value: 24 }
+})();
+
+
 
 // create the connection to database
 const connection = mysql.createConnection({
