e574ff2e0313bf5ee5eca640f223b3fd0d02b863
Ali Ahdab <aliahdab@gmail.com>
2019-12-08 14:27:38 +0100

    ali

--- Week2/app_p1.js
+++ Week2/app_p1.js
@@ -1,15 +1,11 @@
 // get the client
 const mysql = require('mysql2');
 const prompts = require('prompts');
-
-const pool = mysql.createPool({
+const connection = mysql.createConnection({
   host: 'localhost',
   user: 'hyfuser',
   password: 'hyfpassword',
-  database: 'new_world',
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0
+  database: 'new_world'
 });
 
 
@@ -22,32 +18,41 @@ const pool = mysql.createPool({
     type: 'text',
     name: 'languagesSpoken',
     message: 'What is the region? something like (" Nordic Countries")',
-  }],
+  },
+  {
+    type: 'text',
+    name: 'numberCities',
+    message: 'What is the language you want to search for?',
+  }
+  ],
 
   );
-  pool.getConnection(function (err, conn) {
-    // Do something with the connection
-    conn.query(
-      'SELECT city.name FROM city INNER JOIN country ON city.countryCode = country.code where country.name= ? and country.capital=city.id',
-      response.countryName,
-      function (err, results, fields) {
-        console.log('The capital city for ' + response.countryName + ' is', results[0].name);
-      }
-    );
-    pool.releaseConnection(conn);
-  })
+  connection.query(
+    'SELECT city.name FROM city INNER JOIN country ON city.countryCode = country.code where country.name= ? and country.capital=city.id',
+    response.countryName,
+    function (err, results, fields) {
+      console.log('The capital city for ' + response.countryName + ' is', results[0].name);
+    }
+  )
+  connection.query(
+    'Select cl.language from countrylanguage as cl inner join country as co on co.code=cl.countrycode where region=?',
+    response.languagesSpoken,
+    function (err, results, fields) {
+      console.log('---------------------------------------------------------------------------')
+      console.log('The List of languages spoken in the region ' + response.languagesSpoken + ' are', results);
+    }
+  )
+  connection.query(
+    'select count(city.name) as SUM from city inner join countrylanguage as cl on cl.countrycode=city.countrycode where cl.language=?',
+    response.numberCities,
+    function (err, results, fields) {
+      console.log('---------------------------------------------------------------------------')
+      console.log('The number of cities which speak ' + response.numberCities + ' is ', results[0]);
+    }
+  )
 
-  pool.getConnection(function (err, conn) {
-    // Do something with the connection
-    conn.query(
-      'Select cl.language from countrylanguage as cl inner join country as co on co.code=cl.countrycode where region=?',
-      response.languagesSpoken,
-      function (err, results, fields) {
-        console.log('The List of languages spoken in the region ' + response.languagesSpoken + ' are', results);
-      }
-    )
-    pool.releaseConnection(conn);
-  })
+
+  connection.close
 })();
 
 // create the connection to database
