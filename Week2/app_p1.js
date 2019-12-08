4e80ca24743d57d79b2c5017b3f000edc4ab2499
Ali Ahdab <aliahdab@gmail.com>
2019-12-08 12:59:19 +0100

    adding some code

--- Week2/app_p1.js
+++ Week2/app_p1.js
@@ -4,30 +4,38 @@ const prompts = require('prompts');
 
 
 
+
 (async () => {
-  const response = await prompts({
+  const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'hyfuser',
+    password: 'hyfpassword',
+    database: 'new_world'
+  })
+  const response = await prompts([{
     type: 'text',
     name: 'countryName',
+    language: 'languagesSpoken',
     message: 'What is the country name?',
+  }, {
+    type: 'text',
+    name: 'languagesSpoken',
+    message: 'What is the region? something like (" Nordic Countries")',
+  }],
 
-  });
-
-  let countryName = response.countryName;
+  );
   connection.query(
     'SELECT city.name FROM city INNER JOIN country ON city.countryCode = country.code where country.name= ?',
-    countryName,
+    response.countryName,
     function (err, results, fields) {
-      console.log(results[0]); // results contains rows returned by server
+      console.log('The capital city for ' + response.countryName + ' is', results[0].name);
     }
   );
 })();
 
+
+
+
 // create the connection to database
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'hyfuser',
-  password: 'hyfpassword',
-  database: 'new_world'
-})
 
 
